a59db1b26d69380aaedf51a96538549b955b6a98
Andrew Fitzpatrick <andrewfitzpatrick@y7mail.com>
2017-05-21 17:37:13 +1000

    Way more efficient getDigitalRoot function
    - I found this right after making the first one and decided to put it as
    a seperate commit

--- Code Design/source/digitalRoot.h
+++ Code Design/source/digitalRoot.h
@@ -6,27 +6,7 @@ unsigned int numDigits(int input);
 // returns the digital root of a number
 unsigned int getDigitalRoot(unsigned int input)
 {
-    unsigned int total = 0;
-    while (input)
-    {
-        unsigned int power = 1;
-        for (unsigned int i = 1; i < numDigits(input); i++)
-        {
-            power *= 10;
-        }
-        unsigned int currentDigit = input / power;
-        input -= (power * currentDigit);
-        total += currentDigit;
-    }
-
-    if (total < 10)
-    {
-        return total;
-    }
-    else
-    {
-        return(getDigitalRoot(total));
-    }
+    return (input - (9 * ((input - 1) / 9)));
 }
 
 // returns the number of digits in a number
