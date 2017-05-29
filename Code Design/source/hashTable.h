43c774164be8cf7df441fde97d0e0d94fbc24c31
andrewthenintendrone <andrewfitzpatrick@y7mail.com>
2017-05-29 17:01:09 +1000

    100%(ish) working HashTables
    - HashTables now double in size when the Load Factor is more than 70%

--- Code Design/source/hashTable.h
+++ Code Design/source/hashTable.h
@@ -12,13 +12,16 @@ public:
     {
         name.clear();
     }
-    ~Bucket() {}
+    ~Bucket()
+    {
+        name.clear();
+    }
 
     // overloads
     Bucket& operator = (Bucket& otherBucket)
     {
         name = otherBucket.name;
-        data = otherBucket.data;;
+        data = otherBucket.data;
         return *this;
     }
 
@@ -32,83 +35,106 @@ template<typename dataType>
 class HashTable
 {
 public:
+    // default constructor
     HashTable(unsigned int size) : hashFunction(HashFunction::default), m_size(size), m_buckets(new Bucket<dataType>[size]) {}
-    HashTable(HashTable& otherHashTable)
+    
+    // copy constructor
+    HashTable(HashTable& otherHashTable, unsigned int size)
     {
         hashFunction = otherHashTable.hashFunction;
-        m_size = otherHashTable.m_size;
+        m_size = size;
         m_buckets = new Bucket<dataType>[m_size];
-        *this = otherHashTable;
-    }
-
-    ~HashTable() { delete[] m_buckets; }
-
-    // allows access to bucket data
-    dataType& operator [] (const std::string& key)
-    {
-        // hash key
-        unsigned long hashedKey = HashFunction::default((key.c_str())) % m_size;
-        while (true)
-        {
-            // if the hashed key matches the bucket name or the bucket is empty
-            if (m_buckets[hashedKey].name == key || m_buckets[hashedKey].name.empty())
-            {
-                // set the buckets name to key and return it
-                m_buckets[hashedKey].name = key;
-                return m_buckets[hashedKey].data;
-            }
-            // otherwise look at the next button
-            else
-            {
-                if (hashedKey < m_size)
-                {
-                    hashedKey++;
-                }
-                else
-                {
-                    hashedKey = 0;
-                }
-            }
-        }
-    }
-
-    // returns the address of a bucket specified by a key
-    Bucket<dataType>& getBucket(std::string& key)
-    {
-        unsigned long hashedKey = HashFunction::default((key.c_str())) % m_size;
-        while (true)
-        {
-            if (m_buckets[hashedKey].name == key || m_buckets[hashedKey].name.empty())
-            {
-                return m_buckets[hashedKey];
-            }
-            else
-            {
-                if (hashedKey < m_size)
-                {
-                    hashedKey++;
-                }
-                else
-                {
-                    hashedKey = 0;
-                }
-            }
-        }
-    }
-
-    // overloads
-    HashTable& operator = (HashTable& otherHashTable)
-    {
-        // assert that the other Hash Table is the same size or smaller
-        aieASSERT(otherHashTable.m_size <= m_size);
         for (unsigned int i = 0; i < otherHashTable.m_size; i++)
         {
             // ignore empty buckets
-            if (!otherHashTable.m_buckets[i].name.empty())
+            if (!(otherHashTable.m_buckets[i].name.empty()))
             {
                 std::string key = otherHashTable.m_buckets[i].name;
-                getBucket(key) = otherHashTable.getBucket(key);
+                getBucketByKey(key) = otherHashTable.getBucketByKey(key);
             }
+        }
+    }
+
+    // destructor
+    ~HashTable() { clear(); }
+
+    // returns the address of a buckets data
+    dataType& operator [] (const std::string& key)
+    {
+        // resize if load factor becomes too large
+        if (getLoadFactor() > 0.7f)
+        {
+            resize();
+        }
+        // hash key
+        unsigned int hashedKey = hashFunction((key.c_str())) % m_size;
+        while (true)
+        {
+            // if the hashed key matches the bucket name or the bucket is empty
+            if (m_buckets[hashedKey].name.empty())
+            {
+                m_buckets[hashedKey].name = key;
+                return(m_buckets[hashedKey]).data;
+            }
+            else if (m_buckets[hashedKey].name == key)
+            {
+                // set the buckets name to key and return it
+                return m_buckets[hashedKey].data;
+            }
+            // otherwise look at the next bucket
+            else
+            {
+                if (hashedKey < m_size - 1)
+                {
+                    hashedKey++;
+                }
+                else
+                {
+                    hashedKey = 0;
+                }
+            }
+        }
+    }
+
+    // returns the current loadFactor of the HashTable
+    float getLoadFactor()
+    {
+        float usedBuckets = 0;
+        for (unsigned int i = 0; i < m_size; i++)
+        {
+            // ignore empty buckets
+            if (!m_buckets[i].name.empty())
+            {
+                usedBuckets++;
+            }
+        }
+
+        return usedBuckets / m_size;
+    }
+
+    // resizes the HashTable by creating a larger HashTable and transfering to it
+    void resize()
+    {
+        HashTable newHashTable(*this, m_size * 2);
+        *this = newHashTable;
+    }
+
+    // cleans up memory
+    void clear()
+    {
+        delete[] m_buckets;
+    }
+
+    // = overload
+    HashTable& operator = (HashTable& otherHashTable)
+    {
+        clear();
+        m_size = otherHashTable.m_size;
+        m_buckets = new Bucket<dataType>[m_size];
+        for (unsigned int i = 0; i < m_size; i++)
+        {
+            std::string key = otherHashTable.m_buckets[i].name;
+            getBucketByKey(key) = otherHashTable.getBucketByKey(key);
         }
         return *this;
     }
@@ -117,4 +143,31 @@ protected:
     HashFunction::HashFunc hashFunction;
     Bucket<dataType>* m_buckets;
     unsigned int m_size;
+
+    // returns the address of a bucket specified by a key
+    Bucket<dataType>& getBucketByKey(std::string& key)
+    {
+        // hash key
+        unsigned long hashedKey = hashFunction((key.c_str())) % m_size;
+
+        // iterate through buckets until the right one is returned
+        while (true)
+        {
+            if (m_buckets[hashedKey].name == key || m_buckets[hashedKey].name.empty())
+            {
+                return m_buckets[hashedKey];
+            }
+            else
+            {
+                if (hashedKey < m_size - 1)
+                {
+                    hashedKey++;
+                }
+                else
+                {
+                    hashedKey = 0;
+                }
+            }
+        }
+    }
 };
